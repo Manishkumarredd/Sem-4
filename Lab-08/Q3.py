ea3f41f81f8d270867caa04bc5ca67523ed5d6cd
Manish Kumar Reddy <154754455+Manishkumarredd@users.noreply.github.com>
2025-03-30 19:45:45 +0530

    Update Q3.py

--- Lab-08/Q3.py
+++ Lab-08/Q3.py
@@ -1,1 +1,8 @@
+#A3:Different Activation Functions
+activation_functions = ['step', 'bipolar_step', 'sigmoid', 'relu']
+epochs_results = {}
 
+for activation in activation_functions:
+    _, _, epochs = train_perceptron(X_and, y_and, initial_weights.copy(), activation=activation)
+    epochs_results[activation] = epochs
+    print(f"Epochs needed with {activation} activation: {epochs}")
