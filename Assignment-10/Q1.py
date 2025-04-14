88d35aab43ebf004e62f6076ee329c6b703ef1bc
Manish Kumar Reddy <154754455+Manishkumarredd@users.noreply.github.com>
2025-04-14 16:44:28 +0530

    Update Q1.py

--- Assignment-10/Q1.py
+++ Assignment-10/Q1.py
@@ -1,1 +1,17 @@
-print("hello world")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from google.colab import files
+
+uploaded = files.upload()
+
+df=pd.read_excel("219_student.xlsx")
+
+def plot_correlation_heatmap(df):
+    corr = df.corr(numeric_only=True)
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f")
+    plt.title("Feature Correlation Heatmap")
+    plt.show()
+plot_correlation_heatmap(df)
